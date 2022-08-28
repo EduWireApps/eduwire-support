4615b0bea79c24052691a7fc102a19ee84dabce3
JsonLines <45465075+JsonLinesCode@users.noreply.github.com>
2022-08-28 19:25:45 +0200

    :speech_balloon: Update to add Apollo support

--- index.ts
+++ index.ts
@@ -1,60 +1,61 @@
 import { Client, TextChannel, Snowflake, Message, Collection } from 'discord.js';
 import * as config from './config.json';
-import issuesNotif from './issues-pnotif.json';
+import issuesApollo from './issues-apollo.json';
 import issuesYnotes from './issues-ynotes.json';
+import keywords from './keywords.json';
 
 const client = new Client({
-    partials: [ "MESSAGE" ],
+    partials: ["MESSAGE"],
     fetchAllMembers: true
 });
 
 client.login(config.token);
 
-let notifChannel: TextChannel = null;
+let apolloChannel: TextChannel = null;
 let ynotesChannel: TextChannel = null;
-let initialNotifMessageID: Snowflake = null;
+let initialApolloMessageID: Snowflake = null;
 let initialYnotesMessageID: Snowflake = null;
-const ynotesContent = 
-`**Tapez le numéro de votre question et je ferai de mon mieux pour vous aider !**
+const ynotesContent =
+    `**Tapez le numéro de votre question et je ferai de mon mieux pour vous aider !**
 
 ${issuesYnotes.map((issue, index) => {
-    return `\`${++index}\` - ${issue.name}`
-}).join('\n')}`;
+        return `\`${++index}\` - ${issue.name}`
+    }).join('\n')}`;
 
-const notifContent =
-`
+const apolloContent =
+    `
 **Tapez le numéro de votre question et je ferai de mon mieux pour vous aider !**
 
-${issuesNotif.length > 0 ? issuesNotif.map((issue, index) => {
-    return `\`${++index}\` - ${issue.name}`
-}).join('\n') : 'Aucune question disponible pour le moment.'}
+${issuesApollo.length > 0 ? issuesApollo.map((issue, index) => {
+        return `\`${++index}\` - ${issue.name}`
+    }).join('\n') : 'Aucune question disponible pour le moment.'}
 `
 
 client.on('ready', async () => {
     console.log(`Ready. Logged as ${client.user.tag}.`);
-    notifChannel = client.channels.cache.get(config.supportNotifChannel) as TextChannel;
     ynotesChannel = client.channels.cache.get(config.supportYnotesChannel) as TextChannel;
-    notifChannel.messages.fetch().then(async (messages) => {
-        initialNotifMessageID = messages.last()?.id;
-        if(!initialNotifMessageID){
-            initialNotifMessageID = (await notifChannel.send(notifContent)).id;
-        }
-        setInterval(() => {
-            notifChannel.messages.fetch().then((fetchedMessages) => {
-                const messagesToDelete = fetchedMessages.filter((m) => (Date.now() - m.createdTimestamp) > 60000 && m.id !== initialNotifMessageID);
-                notifChannel.bulkDelete(messagesToDelete);
-            });
-        }, 10000);
-    });
     ynotesChannel.messages.fetch().then(async (messages) => {
         initialYnotesMessageID = messages.last()?.id;
-        if(!initialYnotesMessageID){
+        if (!initialYnotesMessageID) {
             initialYnotesMessageID = (await ynotesChannel.send(ynotesContent)).id;
         }
         setInterval(() => {
             ynotesChannel.messages.fetch().then((fetchedMessages) => {
                 const messagesToDelete = fetchedMessages.filter((m) => (Date.now() - m.createdTimestamp) > 60000 && m.id !== initialYnotesMessageID);
                 ynotesChannel.bulkDelete(messagesToDelete);
+            });
+        }, 10000);
+    });
+    apolloChannel = client.channels.cache.get(config.supportApolloChannel) as TextChannel;
+    apolloChannel.messages.fetch().then(async (messages) => {
+        initialApolloMessageID = messages.last()?.id;
+        if (!initialApolloMessageID) {
+            initialApolloMessageID = (await apolloChannel.send(apolloContent)).id;
+        }
+        setInterval(() => {
+            apolloChannel.messages.fetch().then((fetchedMessages) => {
+                const messagesToDelete = fetchedMessages.filter((m) => (Date.now() - m.createdTimestamp) > 60000 && m.id !== initialApolloMessageID);
+                apolloChannel.bulkDelete(messagesToDelete);
             });
         }, 10000);
     });
@@ -84,15 +85,15 @@ const sendAndDeleteAfter = (message: Message, content: string) => {
 
 client.on('messageDelete', (message) => {
     const relatedMessageData = relatedMessages.get(message.id);
-    if(relatedMessageData){
+    if (relatedMessageData) {
         if (relatedMessageData.channelID === ynotesChannel.id) {
             ynotesChannel.messages.fetch(relatedMessageData.messageID).then((m) => {
                 m.delete();
                 clearTimeout(relatedMessageData.timeout);
                 relatedMessages.delete(message.id);
             });
-        } else if (relatedMessageData.channelID === notifChannel.id) {
-            notifChannel.messages.fetch(relatedMessageData.messageID).then((m) => {
+        } else if (relatedMessageData.channelID === apolloChannel.id) {
+            apolloChannel.messages.fetch(relatedMessageData.messageID).then((m) => {
                 m.delete();
                 clearTimeout(relatedMessageData.timeout);
                 relatedMessages.delete(message.id);
@@ -100,12 +101,18 @@ client.on('messageDelete', (message) => {
         }
     }
 })
+function endOfSupportPremadeMessages(input: String) {
+    return (keywords.keywords.some(kw => input.toLowerCase().includes(kw)))
+}
 
 client.on('message', (message) => {
-    if(message.partial) return;
-    if(message.author.bot) return;
-    if(message.channel.id === config.supportYnotesChannel){
-        const answer = issuesYnotes.find((i, indx) => (indx+1).toString() === message.content);
+    if (message.partial) return;
+    if (message.author.bot) return;
+    if (endOfSupportPremadeMessages(message.content)) {
+        message.channel.send(keywords.endofsupport);
+    }
+    if (message.channel.id === config.supportYnotesChannel) {
+        const answer = issuesYnotes.find((i, indx) => (indx + 1).toString() === message.content);
         if (answer) {
             sendAndDeleteAfter(
                 message,
@@ -117,8 +124,8 @@ client.on('message', (message) => {
                 `Bonjour ${message.author.toString()}, veuillez envoyer le numéro du problème que vous rencontrez.`
             );
         }
-    } else if(message.channel.id === config.supportNotifChannel) {
-        const answer = issuesNotif.find((i, indx) => (indx+1).toString() === message.content);
+    } if (message.channel.id === config.supportApolloChannel) {
+        const answer = issuesApollo.find((i, indx) => (indx + 1).toString() === message.content);
         if (answer) {
             sendAndDeleteAfter(
                 message,
